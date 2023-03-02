eacbd0306813148a47ea179d5c98132ff3064f2e
Tommaso Teofili <tteofili@redhat.com>
2023-03-02 12:00:31 +0100

    FAI-948 - debugging line dropped
    Signed-off-by: Tommaso Teofili <tteofili@redhat.com>

--- src/test/java/com/ibm/watson/modelmesh/SidecarModelMeshTest.java
+++ src/test/java/com/ibm/watson/modelmesh/SidecarModelMeshTest.java
@@ -115,7 +115,6 @@ public class SidecarModelMeshTest extends SingleInstanceModelMeshTest {
             // delete
             manageModels.unregisterModel(UnregisterModelRequest.newBuilder()
                     .setModelId(modelId).build());
-            Thread.sleep(100000);
         } finally {
             channel.shutdown();
         }
